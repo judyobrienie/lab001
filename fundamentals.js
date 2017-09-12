aff9ae899167c19707c3ed01bc8ca0337a7c1fce
Judy OBrien <judyobrienie@gmail.com>
2017-09-12 12:19:00 +0100

    Nested objects

--- fundamentals.js
+++ fundamentals.js
@@ -15,3 +15,15 @@ var aCar = {
     console.log(aCar.owner);
     console.log(aCar.owner + ' drives a ' + aCar.type.make);
     console.log(aCar.owner	+ ' Reg. = ' + aCar.registration.year + "-" + aCar.registration.county_code+ "-"+aCar.registration.number);
+
+
+    aCar.milage = "80000";
+    aCar.color = {
+    			exterior : "red",
+    			interior: {
+    				texture: "leather",
+    				shade: "cream"
+    			},
+    }
+
+    console.log("it is a " + aCar.color.exterior + " car, " + aCar.milage + " milage and " + aCar.color.interior.texture + " interior");
